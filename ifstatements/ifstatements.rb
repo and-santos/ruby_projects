fc200624869e010b302149369ce63fbf68ecb66a
and-santos <andsouzasanttos@gmail.com>
2021-10-11 10:23:31 -0300

    ifstatement correction

--- ifstatements/ifstatements.rb
+++ ifstatements/ifstatements.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 =begin
 Logical operators
 and = logical e
@@ -9,15 +8,11 @@ or = logical or
 
 ismale = true
 istall = true
-=======
-ismale = true
->>>>>>> 40c5ce60e5cd951ce32b5cfed25f5ae5b023249e
 
 if ismale
     puts "You are male"
 else
     puts "You are not male"
-<<<<<<< HEAD
 end
 
 if ismale and istall
@@ -29,6 +24,3 @@ elsif !ismale and istall
 else
     puts "You either not male or not tall or both"
 end
-=======
-end
->>>>>>> 40c5ce60e5cd951ce32b5cfed25f5ae5b023249e
