3ca3fe842ae8be284c8c9b2301c856bf9091dd56
and-santos <andsouzasanttos@gmail.com>
2021-10-12 00:44:44 -0300

    commit from andrei pc

--- readingfiles/reading_files.rb
+++ readingfiles/reading_files.rb
@@ -9,5 +9,19 @@
 #e.g. File.open("C:/Users/asantos/files/employees.txt")
 
 File.open("employees.txt", "r") do |file|
+  puts file.readline() #read a line
+  puts file.readline()
+  puts file.readchar() #read a character
   puts file.read()
 end
+
+=begin
+File.open("employees.txt", "r") do |file|
+  for line in file.readlines()
+  end
+end
+=end
+
+file = File.open("employees.txt", "r")
+
+puts file.read
